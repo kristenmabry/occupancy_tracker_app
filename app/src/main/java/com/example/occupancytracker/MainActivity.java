3cca556a0da79a01547b8c3c364d767a3b0bbf13
kristenmabry <kristen@themabryfamily.com>
2022-01-03 15:19:08 -0700

    start main activity

--- app/src/main/java/com/example/occupancytracker/MainActivity.java
+++ app/src/main/java/com/example/occupancytracker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.occupancytracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void viewOccupancy(View view) {
+//        Intent intent = new Intent(this, SendTextActivity.class);
+//        intent.putExtra(SendTextActivity.KEY_IS_NEW, true);
+//        startActivity(intent);
+    }
+
+    public void viewData(View view) {
+//        Intent intent = new Intent(this, ViewLayoutsActivity.class);
+//        startActivity(intent);
+    }
+
+    public void viewDevices(View view) {
+//        Intent intent = new Intent(this, ViewBluetoothDevices.class);
+//        startActivity(intent);
     }
 }
